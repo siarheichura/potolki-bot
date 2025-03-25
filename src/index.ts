8af71b7b3e4f73b282e60238b9c791697161ee71
siarheichura <chura.siarhei@gmail.com>
2025-03-26 00:30:56 +0100

    fix deploy

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,7 @@
 import * as dotenv from 'dotenv';
 dotenv.config();
 
+console.log('ENV10', process.env.SUPABASE_URL);
+
 import { bot } from './bot';
 void bot.launch();
--- src/supabase.ts
+++ src/supabase.ts
@@ -1,5 +1,7 @@
 import { createClient } from '@supabase/supabase-js';
 
+console.log('ENV1', process.env.SUPABASE_URL);
+
 export const supabase = createClient(
   process.env.SUPABASE_URL as string,
   process.env.SUPABASE_ANON_KEY as string,
