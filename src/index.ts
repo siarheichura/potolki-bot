e51632d7ef886423d70a27ca6caf2405b273a0e1
siarheichura <chura.siarhei@gmail.com>
2025-03-26 00:14:07 +0100

    test logs

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,8 @@
 import * as dotenv from 'dotenv';
 dotenv.config();
 
+console.log('ENV', process.env);
+console.log('URL', process.env.SUPDABASE_URL);
+
 import { bot } from './bot';
 void bot.launch();
